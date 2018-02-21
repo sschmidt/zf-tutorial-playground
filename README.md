ed3c09886591fd2c46347d50823b89e6db0523fd
Sebastian Schmidt <sebastian.schmidt@thrivenow.io>
2018-02-21 23:18:14 +0100

    added missing tutorial code

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Getting started
 
 - Run: `docker-compose up`
-- Visit the demo page at `http://localhost:15000`, album list at: `http://localhost:15000/album`
+- Visit the album tutorial at `http://localhost:15000`
 - Find phpMyAdmin at `http://localhost:15001` (user: root, password: root)
 - Find maildog at `http://localhost:15002`
 
--- module/Album/src/Model/Album.php
+++ module/Album/src/Model/Album.php
@@ -2,11 +2,24 @@
 
 namespace Album\Model;
 
+use DomainException;
+use Zend\Filter\StringTrim;
+use Zend\Filter\StripTags;
+use Zend\Filter\ToInt;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterInterface;
+use Zend\Validator\StringLength;
+
+/**
+ * Class Album
+ * @package Album\Model
+ */
 class Album
 {
     public $id;
     public $artist;
     public $title;
+    private $inputFilter;
 
     public function exchangeArray(array $data)
     {
@@ -14,4 +27,89 @@ class Album
         $this->artist = $data['artist'] ?? null;
         $this->title  = $data['title'] ?? null;
     }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(
+            sprintf(
+                '%s does not allow injection of an alternate input filter',
+                __CLASS__
+            )
+        );
+    }
+
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add(
+            [
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => [
+                    ['name' => ToInt::class],
+                ],
+            ]
+        );
+
+        $inputFilter->add(
+            [
+                'name'       => 'artist',
+                'required'   => true,
+                'filters'    => [
+                    ['name' => StripTags::class],
+                    ['name' => StringTrim::class],
+                ],
+                'validators' => [
+                    [
+                        'name'    => StringLength::class,
+                        'options' => [
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $inputFilter->add(
+            [
+                'name'       => 'title',
+                'required'   => true,
+                'filters'    => [
+                    ['name' => StripTags::class],
+                    ['name' => StringTrim::class],
+                ],
+                'validators' => [
+                    [
+                        'name'    => StringLength::class,
+                        'options' => [
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $this->inputFilter = $inputFilter;
+
+        return $this->inputFilter;
+    }
+
+    public function getArrayCopy()
+    {
+        return [
+            'id'     => $this->id,
+            'artist' => $this->artist,
+            'title'  => $this->title,
+        ];
+    }
+
 }
